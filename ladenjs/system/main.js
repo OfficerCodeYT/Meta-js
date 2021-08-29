5ec30183af9358fa9af412a35dbde03301787772
OfficerCodeYT <59301378+OfficerCodeYT@users.noreply.github.com>
2021-08-28 20:32:13 -0400

    Delete ladenjs directory

--- ladenjs/system/main.js
+++
@@ -1,4 +0,0 @@
-const { ladenServer } = require("../http");
-
-console.log("Server Connections : " + ladenServer.connections + "\n")
-console.log("Server Listener Count: " + ladenServer.listenerCount + "\n")
